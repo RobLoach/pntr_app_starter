d6023faae903ff813b1a217c2e1f934ced9d5cdd
Rob Loach <robloach@gmail.com>
2024-07-07 13:56:12 -0400

    Draw the logo

--- src/main.c
+++ src/main.c
@@ -1,10 +1,10 @@
 #define PNTR_APP_IMPLEMENTATION
 #define PNTR_ENABLE_DEFAULT_FONT
-#define PNTR_DISABLE_MATH
 #include "pntr_app.h"
 
 typedef struct AppData {
     pntr_font* font;
+    pntr_image* logo;
 } AppData;
 
 bool Init(pntr_app* app) {
@@ -13,6 +13,7 @@ bool Init(pntr_app* app) {
 
     // Load the default font
     appData->font = pntr_load_font_default();
+    appData->logo = pntr_load_image("resources/logo.png");
 
     return true;
 }
@@ -23,8 +24,13 @@ bool Update(pntr_app* app, pntr_image* screen) {
     // Clear the background
     pntr_clear_background(screen, PNTR_RAYWHITE);
 
+    // Draw the logo
+    if (appData->logo) {
+        pntr_draw_image(screen, appData->logo, screen->width / 2 - appData->logo->width / 2, screen->height / 2 - appData->logo->height / 2);
+    }
+
     // Draw text on the screen
-    pntr_draw_text(screen, appData->font, "Congrats! You created your first pntr_app!", 35, 100, PNTR_DARKGRAY);
+    pntr_draw_text(screen, appData->font, "Congrats! You created your first pntr_app!", 35, screen->height - 30, PNTR_DARKGRAY);
 
     return true;
 }
@@ -32,8 +38,9 @@ bool Update(pntr_app* app, pntr_image* screen) {
 void Close(pntr_app* app) {
     AppData* appData = (AppData*)pntr_app_userdata(app);
 
-    // Unload the font
+    // Unload the assets.
     pntr_unload_font(appData->font);
+    pntr_unload_image(appData->logo);
 
     pntr_unload_memory(appData);
 }
